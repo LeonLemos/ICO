4ac44a3a1a451debf55db8a5dc07d9639dee2c51
Leon Lemos <leonlemos26@gmail.com>
2023-08-05 00:57:24 +0100

    Buy Tokens

---
+++ contracts/Token.sol
@@ -0,0 +1,77 @@
+//SPDX-License-Identifier: UNLICENSE
+pragma solidity ^0.8.9;
+
+import "hardhat/console.sol" ;
+
+contract Token {
+    string public name;
+    string public symbol;
+    uint256 public decimals = 18;
+    uint256 public totalSupply ;
+    
+    //Track Balances
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256))public allowance;
+    constructor(string memory _name, string memory _symbol, uint256 _totalSupply){
+        name = _name;
+        symbol = _symbol;
+        totalSupply= _totalSupply* (10**decimals);
+        balanceOf[msg.sender]= totalSupply;
+    }
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    //Send Tokens
+    function transfer(address _to, uint256 _value) public returns ( bool sucess){
+
+        //Require sender to have enough tokens to spend
+        require(balanceOf[msg.sender]>=_value);
+        
+        _transfer(msg.sender, _to, _value);
+
+        return true;
+    }
+
+    function _transfer(address _from, address _to, uint256 _value) internal {
+
+        //Cant transfer to 0 address
+        require(_to != address(0));
+        //Deduct tokens from spender
+        balanceOf[_from] = balanceOf[_from] - _value;
+        //Credit tokens to receiver
+        balanceOf[_to] = balanceOf[_to] + _value;
+
+        //Emit Event
+        emit Transfer(_from,_to,_value);
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool success){
+        //Cant give permission to 0 address
+        require(_spender != address(0));
+        //Allow spender to spend value
+        allowance[msg.sender][_spender] = _value;
+        
+        //Emit Event
+        emit Approval(msg.sender, _spender, _value);
+
+        return true;
+    }
+
+    function transferfrom(address _from, address _to, uint256 _value) public returns (bool success){
+
+        //Check Approval
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+
+        //Reset Allowance
+        allowance[_from][msg.sender] = allowance[_from][msg.sender] - _value;
+
+        //Spend Tokens
+        _transfer(_from, _to, _value);
+
+        return true;
+
+    }
+}
+
