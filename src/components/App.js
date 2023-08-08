36191c66ef3946b1a3e35ae6b655c4c63e3e5013
Leon Lemos <leonlemos26@gmail.com>
2023-08-08 23:41:39 +0100

    Loads Accounts

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,38 @@
-import {container} from 'react-bootstrap';
+import {useEffect, useState} from 'react';
+
+import {Container} from 'react-bootstrap';
+import {ethers} from 'ethers';
 
 import Navigation from './Navigation';
+import Info from './Info';
+import { useEffect } from 'react';
 
 function App(){
+
+    const [account, setAccount] = useState(null);
+    const loadBlockchainData = async()=>{
+        const provider = new ethers.providers.Web3provider(window.ethereum)
+
+        const accounts = await window.ethereum.request({method:'eth_request_accounts'})
+        const account = ethers.utils.getAddress(accounts[0])
+
+        //Add to state
+        setAccount(account)
+    }
+
+    useEffect(()=>{
+        loadBlockchainData()
+    });
+
+ 
     return(
-        <container>
+        <Container>
             <Navigation/>
-        </container>
+            {/*Read From state*/}
+            <hr/> {/* Only loads account on page when its connected */}
+            {account && (<Info account={account}/>)}
+            
+        </Container>
     )
 }
 
