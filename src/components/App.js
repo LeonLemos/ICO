53fd27d7e9c5f23b4cd7e98b2b057682cbdce5c2
Leon Lemos <leonlemos26@gmail.com>
2023-08-08 12:37:09 +0100

    Navigation Bar

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import {container} from 'react-bootstrap';
+
+import Navigation from './Navigation';
+
+function App(){
+    return(
+        <container>
+            <Navigation/>
+        </container>
+    )
+}
+
+export default App;
